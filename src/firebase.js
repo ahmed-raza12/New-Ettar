a8087ed8d8953d422f14af3c3b3ccc0b30c081f4
ahmed-raza12 <ar907080@gmail.com>
2025-07-12 11:28:24 +0500

    deployed on custom domain

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAuhzILUGNjDkLknvrPBxZ7xMHNg89GSiA",
-  authDomain: "udinfragrance.firebaseapp.com",
-  projectId: "udinfragrance",
-  storageBucket: "udinfragrance.firebasestorage.app",
-  messagingSenderId: "794039995234",
-  appId: "1:794039995234:web:91eddf229771f37745ba3b",
-  measurementId: "G-ZN4LMSHH6X"
+  apiKey: "AIzaSyC2dHnfpx5IW4_StHZq_J2Qf1CKvj6Z2YY",
+  authDomain: "almala-fragrance.firebaseapp.com",
+  databaseURL: "https://almala-fragrance-default-rtdb.firebaseio.com",
+  projectId: "almala-fragrance",
+  storageBucket: "almala-fragrance.firebasestorage.app",
+  messagingSenderId: "289286208273",
+  appId: "1:289286208273:web:70e8a31786c7d2666caab0",
+  measurementId: "G-737Y7DH729"
 };
 
 // Initialize Firebase
